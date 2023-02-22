c9dd40861398715810427d9b50e18de4c6caa5c5
Metin Kaim <metinkaim0@gmail.com>
2023-02-22 22:08:23 +0300

    Enemy prefab and navigation
    enemy prefab oluşturduk ve AI navigation navmesh agent verildi

---
+++ UdemyProject3/Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyProject3.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        
+    }
+}
